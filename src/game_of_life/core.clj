b3fe170dc7d255ad4a3d264a9b412d180f480718
Nathan Kear <n.j.kear@gmail.com>
2019-12-02 19:23:55 +1300

    Count cell neighbours

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,6 +1,32 @@
 (ns game-of-life.core
   (:gen-class))
 
+(def test-map [[1 0 1] [1 1 1] [1 0 0]])
+(def cell-width 10)
+(def grid-columns 20)
+(def grid-rows 20)
+(def neighbour-offsets
+  (list [1 1] [1 0] [1 -1] [0 1] [0 -1] [-1 1] [-1 0] [-1 -1]))
+
+(defn point-to-screen-rect [row column]
+  [(* row cell-width) (* column cell-width) cell-width cell-width])
+
+(defn add-offset [[row column]]
+    (fn [[y-offset x-offset]] 
+      [(+ row y-offset) (+ column x-offset)]))
+
+(defn is-alive? [cell-map]
+  (fn [[row column]]
+    (= 1 
+      (get-in cell-map [row column]))))
+
+(defn count-cell-neighbours [cell-map row column]
+  (count 
+    (filter true? 
+      (map 
+        (is-alive? cell-map) 
+          (map (add-offset [row column]) neighbour-offsets)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -2,6 +2,61 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest point-to-screen-rect-tests 
+  (testing "point-to-screen-rect."
+    (testing "Natural integer coordinates."
+      (is (= [0 0 10 10] (point-to-screen-rect 0 0)))
+      (is (= [20 30 10 10] (point-to-screen-rect 2 3))))
+    (testing "Negative integer coordinates."
+      (is (= [-50 -30 10 10] (point-to-screen-rect -5 -3)))
+      (is (= [20 -30 10 10] (point-to-screen-rect 2 -3))))))
+
+; (deftest increment-cell-state-tests
+;   (testing "increment-cell-state."
+;     (testing "Alive cell." 
+;       (testing "Dies."
+;         (is (= 0 (increment-cell-state 1 0)))
+;         (is (= 0 (increment-cell-state 1 1)))
+;         (is (= 0 (increment-cell-state 1 4)))
+;         (is (= 0 (increment-cell-state 1 5)))
+;         (is (= 0 (increment-cell-state 1 6)))
+;         (is (= 0 (increment-cell-state 1 7)))
+;         (is (= 0 (increment-cell-state 1 8))))
+;       (testing "Remains alive."
+;         (is (= 1 (increment-cell-state 1 2)))
+;         (is (= 1 (increment-cell-state 1 3)))))
+;     (testing "Dead cell."
+;       (testing "Remains dead."
+;         (is (= 0 (increment-cell-state 1 0)))
+;         (is (= 0 (increment-cell-state 1 1)))
+;         (is (= 0 (increment-cell-state 1 2)))
+;         (is (= 0 (increment-cell-state 1 4)))
+;         (is (= 0 (increment-cell-state 1 5)))
+;         (is (= 0 (increment-cell-state 1 6)))
+;         (is (= 0 (increment-cell-state 1 7)))
+;         (is (= 0 (increment-cell-state 1 8))))
+;       (testing "Comes alive."
+;         (is (= 1 (increment-cell-state 1 3)))))))
+
+(deftest is-alive?-tests
+  (let [test-grid [[1 0 1] [1 1 1] [0 0 1]]]
+    (testing "is-alive?"
+      (testing "Coordinate on grid." 
+        (is (= true ((is-alive? test-grid) [0 0])))
+        (is (= true ((is-alive? test-grid) [1 2])))
+        (is (= false ((is-alive? test-grid) [2 1])))
+        (is (= false ((is-alive? test-grid) [0 1])))))))
+
+(deftest add-offset-tests
+  (testing "add-offset."
+      (is (= [4 6] ((add-offset [1 2]) [3 4])))
+      (is (= [0 0] ((add-offset [-3 2]) [3 -2])))))
+
+(deftest count-cell-neighbours-tests
+  (testing "count-cell-neighbours."
+    (is (= 0 (count-cell-neighbours [[0 0 0] [0 1 0] [0 0 0]] 1 1)))
+    (is (= 4 (count-cell-neighbours [[1 0 0] [0 1 1] [1 1 0]] 1 1)))
+    (is (= 8 (count-cell-neighbours [[1 1 1] [1 1 1] [1 1 1]] 1 1)))
+    (is (= 8 (count-cell-neighbours [[1 1 1] [1 0 1] [1 1 1]] 1 1)))
+    (is (= 2 (count-cell-neighbours [[1 1 1] [1 0 1] [1 1 1]] 0 0)))
+    (is (= 2 (count-cell-neighbours [[1 1 1] [1 0 1] [1 1 1]] 2 2)))))
